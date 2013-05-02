921a81aa034685396806ac9624110f2e76334839
Logan <kai.ralston@gmail.com>
2013-05-02 11:24:56 -0300

    Create game.php

---
+++ classes/game.php
@@ -0,0 +1,9 @@
+class Game {
+	public $name; // The games name
+	public $link_game; // The link to the game
+	public $link_code; // The link to the gams's code
+	public $leaders; // The game's lead developers
+	pulbic $developers; // The game's developers
+	public $favs; // Users who have favorited the game
+	public $status; // The games status: "Alpha", "Beta", or "Complete"
+}
