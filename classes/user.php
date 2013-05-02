7666e49fd8560ddc3f848aea4275190314c224bb
Logan <kai.ralston@gmail.com>
2013-05-02 19:33:11 -0300

    Remove ideas

--- classes/user.php
+++ classes/user.php
@@ -3,6 +3,5 @@ class User {
 	public $about; // About the user, for profile
 	public $score; // User's rank on the site
 	public $games; // Games user has helped create
-	public $ideas; // Ideas user has created
 	pulbic $favs; // Favorite games and ideas
 }
