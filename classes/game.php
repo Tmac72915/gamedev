d937f1c9c1316048cfa1ee446dd48a08a870f529
Logan <kai.ralston@gmail.com>
2013-05-02 11:28:48 -0300

    Fix bug

--- classes/game.php
+++ classes/game.php
@@ -5,7 +5,7 @@ class Game {
 	public $link_game; // The link to the game
 	public $link_code; // The link to the gams's code
 	public $leaders; // The game's lead developers
-	pulbic $developers; // The game's developers
+	public $developers; // The game's developers
 	public $favs; // Users who have favorited the game
 	public $status; // The games status: "Alpha", "Beta", or "Complete"
 }
