18ab08e76c70aee68588f49b9c1a40ff6e0af44f
Logi0 <kai.ralston@gmail.com>
2013-05-02 17:59:32 -0400

    Add login button

--- static/js/main.js
+++ static/js/main.js
@@ -3,3 +3,8 @@
 $( "html" ).removeClass( "no-js" );
 
 })( jQuery );
+
+var signinLink = document.getElementById( 'signin' );
+if ( signinLink ) {
+  signinLink.onclick = function() { navigator.id.request(); };
+}
--- views/header.php
+++ views/header.php
@@ -18,6 +18,7 @@ if ( !defined( 'NAV_ID' ) ) {
 				<a href="#"<?php if ( NAV_ID === 'ideas' ) { echo ' class="active"'; } ?>>Ideas</a>
 				<a href="learn.php"<?php if ( NAV_ID === 'learn' ) { echo ' class="active"'; } ?>>Learn</a>
 				<a href="#"<?php if ( NAV_ID === 'create' ) { echo ' class="active"'; } ?>>Create</a>
+				<a><div id="signin">Sign in</div></a>
 			</nav>
 		</div>
 	</header>
