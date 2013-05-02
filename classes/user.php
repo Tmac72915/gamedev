c3bfaaeb7333fc8cc2a3a24d2bb7f98a9c322c39
Logan <kai.ralston@gmail.com>
2013-05-02 11:16:16 -0300

    Create user.php

---
+++ classes/user.php
@@ -0,0 +1,8 @@
+class User {
+	public $username; // The user's username
+	public $about; // About the user, for profile
+	public $score; // User's rank on the site
+	public $games; // Games user has helped create
+	public $ideas; // Ideas user has created
+	pulbic $favs; // Favorite games and ideas
+}
