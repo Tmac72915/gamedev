717542c0980860e69c321e8bfe6be962ba2add66
Logan <kai.ralston@gmail.com>
2013-05-02 19:03:29 -0300

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,8 +3,3 @@
 $( "html" ).removeClass( "no-js" );
 
 })( jQuery );
-
-var signinLink = document.getElementById( 'signin' );
-if ( signinLink ) {
-  signinLink.onclick = function() { navigator.id.request(); };
-}
