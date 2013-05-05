6be5093fc3af08c32c4725d0161f7999cf45f917
Logan <kai.ralston@gmail.com>
2013-05-05 15:29:07 -0300

    Create games.php

---
+++ games.php
@@ -0,0 +1,32 @@
+<?php
+require_once( 'main.php' );
+
+define( 'NAV_ID', 'games' );
+
+include_once( VIEWS . 'head.php' );
+?>
+<body>
+
+<?php
+include_once( VIEWS . 'header.php' );
+?>
+
+  <div class="container tm30 well">
+	    <header class="header">
+			Try one of these awesome HTML5 games!
+		</header>
+		<div class="cell">
+		    <h2>Vilify</h2>
+		    <img src="http://placehold.it/250x170">
+		    <p>
+		        You are a mad scientist, who has just completed a doomsday device! In 24 hours the device will erupt, destroying the world! Unfortunately, those pesky Super Heroes have discovered your evil plan, and now they are invading your lab! 
+		    </p>
+		</div>
+	</div>
+
+<?php
+include_once( VIEWS . 'footer.php' );
+?>
+
+</body>
+</html>
